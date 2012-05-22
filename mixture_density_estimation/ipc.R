56f5a0ae79eb4d7aebf8eea3b3efd41685428f1b
natorro <carlos.nataren@gmail.com>
2012-05-22 03:09:01 -0500

    Final version of the analysis

--- mixture_density_estimation/ipc.R
+++ mixture_density_estimation/ipc.R
@@ -6,8 +6,28 @@ library(Hmisc)
 library(tseries)
 library(nortest)
 
+# The EM function that assumes that both normal densities are centered on 0
+em <- function(datos, params)
+{
+  paramsP <- c(-9,-9,-9)
+  while(sum(paramsP-params)^2 > (0.001^2))
+  {
+    paramsP <- params
+    #E
+    p=params[1] * dnorm(datos, sd=params[2])/
+      (params[1] * dnorm(datos, sd=params[2]) + 
+      (1 - params[1]) * dnorm(datos, sd=params[3]))
+    #M
+    params[1] = mean(p)
+    params[2] = sqrt(sum(p * datos^2) / sum(p))
+    params[3] = sqrt(sum((1 - p) * datos^2) / sum(1 - p))
+  }
+  params_list <- list("alfa"=params[1], "sigma_zero"=params[2], "sigma_one"=params[3])
+  return(params_list)
+}
+
 # 5 years
-ipc <- yahooSeries("^MXX", from="2007-05-18", to="2012-05-18")
+ipc <- yahooSeries("^MXX", from="2007-05-21", to="2012-05-21")
 
 names(ipc) <- c("MXX.Open", "MXX.High", "MXX.Low", "MXX.Close", "MXX.Volume", "MXX.Adj.Close")
 
@@ -20,7 +40,8 @@ length(log_rate_of_returns)
 # TODO: Check the autocorrelation structure 
 # ACF(as.ts(rate_of_returns))
 
-ipc_values_EM <- normalmixEM(log_rate_of_returns, arbvar = TRUE, arbmean=FALSE, epsilon = 1e-03, mu=c(0, 0), sigma=c(sd(log_rate_of_returns), sd(log_rate_of_returns)+0.4))
+# Calculate the values for the model
+ipc_values_EM <- normalmixEM(log_rate_of_returns, arbvar = TRUE, arbmean=FALSE, epsilon = 1e-03, mu=c(0,0), sigma=c(sd(log_rate_of_returns), sd(log_rate_of_returns)+0.4))
 ipc_values_EM2 <- normalmixEM2comp(log_rate_of_returns, lambda=0.5, mu=c(mean(log_rate_of_returns), mean(log_rate_of_returns)), sigsqrd=c(sd(log_rate_of_returns), sd(log_rate_of_returns)+4))
 
 summary(log_rate_of_returns)
@@ -35,15 +56,9 @@ mymean <- mean(log_rate_of_returns)
 # Graph 01
 # Plot the histogram with the curve of the normal distribution using the mean and standar deviation from the data
 title <- paste("Histograma de log-retornos y normal con \nmedia= ", mymean, "\ndesviación estándar= ", mydeviation)
-hist(log_rate_of_returns, prob=TRUE, breaks=50, main=title,
+hist(log_rate_of_returns, prob=TRUE, breaks=25, main=title,
      xlab="log-retornos", ylab="Densidad")
 curve(dnorm(x, mymean, mydeviation), from=-0.2, to=0.2, add=TRUE, col="red")
-
-
-# Recreate a normal vector with the same number of elements as the log-returns
-# using the same mean and sd from the data
-
-mynormal <- rnorm(length(log_rate_of_returns), mean(log_rate_of_returns), sd(log_rate_of_returns))
 
 # Jarque Bera Normality Test
 jarque.bera.test(log_rate_of_returns)
@@ -55,7 +70,7 @@ shapiro.test(log_rate_of_returns)
 ad.test(log_rate_of_returns)
 
 # Kolmogorov-Smirnoff Goodness of Fit Test 
-ks.test(log_rate_of_returns, mynormal)
+ks.test(log_rate_of_returns, pnorm)
 
 # Graph 02
 # Let's plot the qqplot supposing a normal distribution and a line 
@@ -65,13 +80,68 @@ qqline(log_rate_of_returns, col="red")
 
 # Graph 03
 # Let's plot the ecdf and ecdf of the normal distribution with same mean and sd from data
-Ecdf(log_rate_of_returns, lty=1, lwd=2, col="green", main="Tasa de retornos")
-Ecdf(mynormal, lty=2, lwd=2, col="red", add=TRUE)
+Ecdf(log_rate_of_returns, lty=1, lwd=2, col="blue", main="Tasa de retornos")
+curve(pnorm(x, mean(log_rate_of_returns), sd(log_rate_of_returns)),
+      from=-0.10, to=0.07, add=TRUE, col="red")
 
-# Generate random numbers from the mixture
-mymixturedata <- rnormmix(length(log_rate_of_returns), lambda=ipc_values_EM$lambda, 
-                          mu=ipc_values_EM$mu, sigma=ipc_values_EM$sigma)
 
-Ecdf(log_rate_of_returns, lty=1, lwd=2, col="green", main="Tasa de retornos")
-Ecdf(mymixturedata, lty=2, lwd=2, col="red", add=TRUE)
+# This is done only if I need to plot the ecdf from the simulated sample
+#Ecdf(mynormal, lty=2, lwd=2, col="red", add=TRUE)
+
+# Calculate values for EM assuming centered in zero
+values <- em(log_rate_of_returns, 
+             c(0.5, 
+               sd(log_rate_of_returns), 
+               4*sd(log_rate_of_returns)))
+
+values
+
+# Graph 04
+Ecdf(log_rate_of_returns, lty=1, lwd=2, col="blue", main="Tasa de retornos")
+
+curve(values$alfa * pnorm(x, 0, values$sigma_zero) +
+      (1-values$alfa) * pnorm(x, 0, values$sigma_one),
+      from=-0.10, to=0.07, add=TRUE, col="red")
+
+curve(ipc_values_EM$lambda[1] * pnorm(x, ipc_values_EM$mu[1], ipc_values_EM$sigma[1]) 
+  + ipc_values_EM$lambda[2] * pnorm(x, ipc_values_EM$mu[2], ipc_values_EM$sigma[2]), 
+      from=-0.10, to=0.07, add=TRUE, col="green")
+
+
+
+# Graph 05 
+title <- paste("Histograma de log-retornos y  \ndos componentes de la mezcla y la mezcla")
+hist(log_rate_of_returns, prob=TRUE, breaks=25, main=title,
+     xlab="log-retornos", ylab="Densidad", ylim=c(0,45))
+curve(ipc_values_EM$lambda[1]*dnorm(x, ipc_values_EM$mu[1], ipc_values_EM$sigma[1]), 
+      from=-0.10, to=0.07, add=TRUE, col="blue", ylim=c(0,45))
+curve(ipc_values_EM$lambda[2]*dnorm(x, ipc_values_EM$mu[2], ipc_values_EM$sigma[2]), 
+      from=-0.10, to=0.07, add=TRUE, col="blue", ylim=c(0,45))
+curve(ipc_values_EM$lambda[1] * dnorm(x, ipc_values_EM$mu[1], ipc_values_EM$sigma[1]) +
+      ipc_values_EM$lambda[2] * dnorm(x, ipc_values_EM$mu[2], ipc_values_EM$sigma[2]), 
+      from=-0.10, to=0.07, add=TRUE, col="red", ylim=c(0,45))
+
+#Graph 06 
+title <- paste("Histograma de log-retornos y  \ndos componentes de la mezcla\ny la mezcla con mu=0")
+hist(log_rate_of_returns, prob=TRUE, breaks=25, main=title,
+     xlab="log-retornos", ylab="Densidad", ylim=c(0,45))
+
+curve(values$alfa*dnorm(x, 0, values$sigma_zero), 
+      from=-0.10, to=0.07, add=TRUE, col="blue", ylim=c(0,45))
+
+curve((1-values$alfa)*dnorm(x, 0, values$sigma_one), 
+      from=-0.10, to=0.07, add=TRUE, col="blue", ylim=c(0,45))
+
+curve(values$alfa * dnorm(x, 0, values$sigma_zero) +
+      (1-values$alfa) * dnorm(x, 0, values$sigma_one),
+      from=-0.10, to=0.07, add=TRUE, col="red", ylim=c(0,45))
+
+# Hypothesis testing for this
+pmnorm <- function(x, mu, sigma, alfa) {
+  alfa[1]*pnorm(x,mu[1],sigma[1]) + (1-alfa[1])*pnorm(x,mu[2],sigma[2])
+}
+
+ks.test(log_rate_of_returns, pmnorm, ipc_values_EM$mu, ipc_values_EM$sigma, ipc_values_EM$lambda)
+ks.test(log_rate_of_returns, pmnorm, c(0, 0), c(values$sigma_zero, values$sigma_one), c(values$alfa, 1-values$alfa))
+
 
